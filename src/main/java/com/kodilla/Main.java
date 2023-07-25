a885161d4ef5f9d441936c35f3f58332985c3534
Euge <euge.itech@gmail.com>
2023-07-25 20:03:17 +0200

    . Krótko o Javie i kodowaniu

--- src/main/java/com/kodilla/Main.java
+++ src/main/java/com/kodilla/Main.java
@@ -2,6 +2,6 @@ package com.kodilla;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Oto mój pierwszy kod!");
     }
 }
