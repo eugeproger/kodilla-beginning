0beac37aea0d9546698b3885e2ad00f1e3e46f8a
Euge <euge.itech@gmail.com>
2023-08-02 19:00:46 +0200

    pouse

---
+++ src/main/java/com/kodilla/modul1/submodul4/jdp/Bank.java
@@ -0,0 +1,16 @@
+package com.kodilla.modul1.submodul4.jdp;
+
+abstract class Bank {
+
+    public void informAcceptanceWithdrawRequest() {
+        System.out.println("A withdrawal request is acceptance");
+    }
+
+    public void checkAccountBalance() {
+        System.out.println("Checking your account balance");
+    }
+
+    public void confirmOfThePossibilityOfWithdrawal() {
+
+    }
+}
