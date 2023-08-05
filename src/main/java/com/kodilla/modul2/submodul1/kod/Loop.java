d46b861863a857a1c7e868fffee99d2ef815ae5a
Euge <euge.itech@gmail.com>
2023-08-05 15:10:36 +0200

    2.1. Pętla for

---
+++ src/main/java/com/kodilla/modul2/submodul1/kod/Loop.java
@@ -0,0 +1,19 @@
+package com.kodilla.modul2.submodul1.kod;
+
+public class Loop {
+
+    public static void main(String[] args) {
+        String[] names = new String[5];
+        names[0] = "Cat";
+        names[1] = "Dog";
+        names[2] = "Horse";
+        names[3] = "Elephant";
+        names[4] = "Tiger";
+
+        System.out.println("Array element names[0] = " + names[0]);
+        System.out.println("Array element names[1] = " + names[1]);
+        System.out.println("Array element names[2] = " + names[2]);
+        System.out.println("Array element names[3] = " + names[3]);
+        System.out.println("Array element names[4] = " + names[4]);
+    }
+}
