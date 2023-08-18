288aae3e082e64f318bce71a1fc4bfe3e36b2e4b
Euge <euge.itech@gmail.com>
2023-08-18 20:16:33 +0200

    pouse

---
+++ src/main/java/com/kodilla/modul2/submodul6/jdp/App.java
@@ -0,0 +1,53 @@
+package com.kodilla.modul2.submodul6.jdp;
+
+import java.util.*;
+
+public class App {
+
+    public static void main(String[] args) {
+        Random random = new Random();
+
+        Map<Student, List<Integer>> group = new HashMap<>();
+        for (int i = 0; group.size() < 20; i++) {
+
+            String name = "Student";
+            int id = random.nextInt(88889)+10000;
+            Student student = new Student(id, name);
+
+            List<Integer> grades = new ArrayList<>();
+            for (int n = 0; grades.size() < 20; n++) {
+                grades.add(random.nextInt(6)+1);
+            }
+            group.put(student, grades);
+        }
+
+        Map<Student, List<Integer>> group2 = new HashMap<>();
+
+        Student student1 = new Student(1, "Alan");
+        Student student2 = new Student(2, "Max");
+
+        List<Integer> grades1 = new ArrayList<>();
+        grades1.add(2);
+        grades1.add(3);
+
+        List<Integer> grades2 = new ArrayList<>();
+        grades2.add(4);
+        grades2.add(5);
+
+        group2.put(student1, grades1);
+        group2.put(student2, grades2);
+
+        for (Map.Entry<Student, List<Integer>> entry : group.entrySet()) {
+            System.out.print(entry.getKey());
+            for (int grade: entry.getValue()) {
+                System.out.print(" " + grade + " ");
+            }
+            System.out.println(" ");
+        }
+
+
+
+
+
+    }
+}
